b360ea15ee2e95aac7f1a4444f727f2b7f92e3d9
root <kakashi.Mamun@gmail.com>
2013-09-24 08:34:34 +0600

    DataCom Server updating

---
+++ public_html/LoginServer/sql/Database Size and Info/1.sql
@@ -0,0 +1,9 @@
+/*Find total number of tables,rows,total data, total index size for a given mysql instance*/
+SELECT COUNT(*) tables,
+  concat(round(sum(table_rows)/1000000,2),'M')rows,
+  concat(round(sum(data_length)/(1024*1024),2),'M') data,
+  concat(round(sum(index_length)/(1024*1024),2),'M') idx,
+  concat(round(sum(data_length+index_length)/(1024*1024),2),'M') total_size,
+  round(sum(index_length)/sum(data_length),2) index_fraction
+FROM information_schema.TABLES;
+
